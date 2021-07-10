b86c317ab87c952da683387aa6d6fbad9f53f87d
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-10 14:01:55 -0300

    init tsc and introduction

---
+++ Introducao/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function sum(num1, num2) {
+  return num1 + num2;
+}
+var myNum1 = 10;
+var myNum2 = 5;
+var resultado = sum(myNum1, myNum2);
+console.log(resultado);
---
+++ Introducao/index.ts
@@ -0,0 +1,10 @@
+function sum(num1:number, num2: number) {
+    return num1 + num2;
+}
+
+const myNum1 = 10;
+const myNum2 = 5;
+
+const resultado = sum(myNum1, myNum2);
+
+console.log(resultado);
