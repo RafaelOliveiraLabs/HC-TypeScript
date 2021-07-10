e3803868dfda1e71c281e32832dbfb0b03a32224
rafamoliv <rafaoliv.ti@gmail.com>
2021-07-10 15:33:38 -0300

    Heranca

--- ModificadoresAcesso e Heranca/modificadoresAcesso.ts
+++ ModificadoresAcesso e Heranca/modificadoresAcesso.ts
@@ -1,3 +1,4 @@
+//Modificadores de acesso
 class Carro {
     private velocidadeAtual: number = 0;
 
@@ -28,3 +29,21 @@ class Carro {
 
 const carro = new Carro('Chevrolet', 'Prisma', 250)
 carro.acelerar();
+
+//Herança
+class Camaro extends Carro {
+    private turbo = false;
+
+    constructor() {
+        super('Chevrolet', 'Camaro', 500)
+    }
+
+    ligarTurbo(){
+        this.turbo = true;
+    }
+}
+
+const camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
